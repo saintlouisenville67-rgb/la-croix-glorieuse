778b03f2efdd10c48d1c508509340359c9bccbbe
saintlouisenville67-rgb <saintlouisenville67@gmail.com>
2026-03-04 17:49:55 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,9 @@ const SUPABASE_URL = "https://rixdjcjlepadtchctxnn.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpeGRqY2psZXBhZHRjaGN0eG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2MTQ2MzMsImV4cCI6MjA4MzE5MDYzM30.0rcj8iFYw-kK7RbAAJ7ZbYqCTdHIWV1DmIQtKP_Ky4c";
 
 // Initialisation du client principal
+if (typeof supabase === 'undefined') {
+    console.error('config.js: La librairie @supabase/supabase-js doit être chargée avant config.js');
+}
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 
